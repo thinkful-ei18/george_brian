efd319a752c49973e59b8efb05be92b68ce3eb22
George Luke <george.luke92@gmail.com>
2018-01-18 16:11:24 -0600

    added HTML stuff to jQuery

--- main.js
+++ main.js
@@ -5,18 +5,27 @@ function addToShoppingList(event) {
   $('#js-shopping-list-form').submit(event => {
     event.preventDefault();
     $('ul').append(
-      '<li>' + $('.js-shopping-list-entry').val().addClass('.shopping-list > li') + '</li>'
-    );
-    console.log($('ul li'));
-  });
-  
-//.submit(), preventDefault(), toggleClass(), and closest().
-  // $('ul').on('click', 'li', function(event) {
-  //   this.remove();
-  // });
+      '<li>' + $('.js-shopping-list-entry').val() + 
+      '<div class="shopping-item-controls">' + 
+        '<button class = "shopping-item-toggle">' +
+        '<span class="button-label"> check </span>' +
+         '</button>' +
+         '<button class = "shopping-item-delete">' +
+         '<span class="button-label"> delete </span>' +
+          '</button>' + '</div>' + '</li>');
+    //.addClass('.shopping-list > li'/* 'shopping-item-controls', 'shopping-item-toggle', 'button-label', 'button-label', 'shopping-item-delete'*/)
+  }
+  );
 }
 
-  $(addToShoppingList());
+  
+//.submit(), preventDefault(), toggleClass(), and closest().
+// $('ul').on('click', 'li', function(event) {
+//   this.remove();
+// });
+
+
+$(addToShoppingList());
 //User can remove item
 
 //User can check and uncheck item
