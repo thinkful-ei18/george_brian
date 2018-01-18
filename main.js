84c08f95e252549f6e0e124952f1db2ed02daf2e
George Luke <george.luke92@gmail.com>
2018-01-18 17:44:58 -0600

    a bunch of stuff here

--- main.js
+++ main.js
@@ -2,23 +2,27 @@
 'use strict';
 
 function addToShoppingList(event) {
+  $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+    $(this).parent().parent().remove();
+  });
+  $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+    $(this).parent().parent().find('.shopping-item').toggleClass('shopping-item__checked');
+  });
   $('#js-shopping-list-form').submit(event => {
     event.preventDefault();
-    $('ul').append(
-      '<li>' + $('.js-shopping-list-entry').val() + $('.shopping-list li').clone().insertAfter('.shopping-list li') + '</li>');
-    // '<div class="shopping-item-controls">' + 
-    //   '<button class = "shopping-item-toggle">' +
-    //   '<span class="button-label"> check </span>' +
-    //    '</button>' +
-    //    '<button class = "shopping-item-delete">' +
-    //    '<span class="button-label"> delete </span>' +
-    //     '</button>' + '</div>' + '</li>');
+    $('.shopping-list').append(`<li>   
+      <span class="shopping-item"> ${$('.js-shopping-list-entry').val()} </span> 
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+        <span class="button-label"> check </span> </button> 
+        <button class="shopping-item-delete">
+        <span class="button-label"> delete </span> </button> </div> </li>`);
     //.addClass('.shopping-list > li'/* 'shopping-item-controls', 'shopping-item-toggle', 'button-label', 'button-label', 'shopping-item-delete'*/)
-  }
-  );
+  });
 }
 
-  
+//$('.shopping-list li').clone().insertAfter(".shopping-list li");
+
 //.submit(), preventDefault(), toggleClass(), and closest().
 // $('ul').on('click', 'li', function(event) {
 //   this.remove();
